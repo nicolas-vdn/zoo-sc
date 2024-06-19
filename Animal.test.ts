df45c0a5a8ca9fa210b3b7ec54265b24f3eb77df
Nicolas Van-Duysen <nicolas.ssj.vdn@gmail.com>
2024-06-19 17:09:16 +0200

    WIP 1ère série de tests - Animal vide créé - Piloté par BF

--- Animal.test.ts
+++ Animal.test.ts
@@ -8,6 +8,26 @@ describe("Un animal vide est créé, tous les attributs existent et sont vides",
   });
 
   it("L'animal est défini", () => {
-    expect(animalVide).not.toBe(undefined);
+    expect(animalVide).not.toBe(null);
+  });
+
+  it("L'animal contient l'attribut identifiant", () => {
+    expect(animalVide.getIdentifiant()).not.toBe(null);
+  });
+
+  it("L'animal contient l'attribut nom", () => {
+    expect(animalVide.getNom()).not.toBe(null);
+  });
+
+  it("L'animal contient l'attribut espèce", () => {
+    expect(animalVide.getEspece()).not.toBe(null);
+  });
+
+  it("L'animal contient l'attribut âge", () => {
+    expect(animalVide.getAge()).not.toBe(null);
+  });
+
+  it("L'animal contient l'attribut enclos", () => {
+    expect(animalVide.getEnclos()).not.toBe(null);
   });
 });
--- Animal.ts
+++ Animal.ts
@@ -1,3 +1,53 @@
+import { v4 as uuidv4 } from "uuid";
+
 export class Animal {
-  constructor() {}
+  private identifiant: string;
+  private nom: string;
+  private espece: string;
+  private age: number;
+  private enclos: string;
+
+  constructor() {
+    this.identifiant = uuidv4();
+    this.nom = "";
+    this.espece = "";
+    this.age = 0;
+    this.enclos = "";
+  }
+
+  public getIdentifiant(): string {
+    return this.identifiant;
+  }
+
+  public getNom(): string {
+    return this.nom;
+  }
+
+  public setNom(nom: string): void {
+    this.nom = nom;
+  }
+
+  public getEspece(): string {
+    return this.espece;
+  }
+
+  public setEspece(espece: string): void {
+    this.espece = espece;
+  }
+
+  public getAge(): number {
+    return this.age;
+  }
+
+  public setAge(age: number): void {
+    this.age = age;
+  }
+
+  public getEnclos(): string {
+    return this.enclos;
+  }
+
+  public setEnclos(enclos: string): void {
+    this.enclos = enclos;
+  }
 }
